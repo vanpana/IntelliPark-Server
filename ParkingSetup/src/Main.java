a886f67bfcc179512761291fa85cb3c8738cb8f9
rocaeric <eric.roca@student.utcluj.ro>
2017-11-04 16:09:48 +0200

    Almost completed matrix

--- ParkingSetup/src/Main.java
+++ ParkingSetup/src/Main.java
@@ -1,9 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
+import java.util.ArrayList;
+
+import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseListener;
 
 public class Main {
+    private static List<ParkingBlock> blocksList;
 
     private static void createAndShowGUI() {
+        blocksList = new ArrayList<>();
+
         JFrame frame = new JFrame("Setup");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
@@ -24,48 +33,79 @@ public class Main {
         rowField.setBounds(160, 20, 40, 20);
         columnField.setBounds(160, 60, 40, 20);
 
-        JButton okButton = new JButton("Generate");
+        CanvasPanel canvas = new CanvasPanel(blocksList);
+        frame.add(canvas);
+        canvas.setBounds(300, 160, 1600, 900);
+        canvas.setVisible(true);
+
+        JButton okButton = new JButton("Ok");
         okButton.addActionListener((e) -> {
             int rows = Integer.parseInt(rowField.getText());
             int columns = Integer.parseInt(columnField.getText());
 
-            CanvasPanel canvas = new CanvasPanel(rows, columns);
-            frame.add(canvas);
-            canvas.setBounds(300, 160, 1600, 900);
-            canvas.setVisible(true);
+            blocksList = new ArrayList<>();
+            int x;
+            int y = 20;
+            for (int i = 0; i < rows; ++i) {
+                x = 20;
+                for (int j = 0; j < columns; ++j) {
+                    x += 21;
+                    blocksList.add(new ParkingBlock(x, y));
+                }
+                y += 21;
+            }
+
+            canvas.setBlocksList(blocksList);
             canvas.updateGraphics();
         });
         frame.add(okButton);
         okButton.setBounds(20, 100, 100, 20);
+
+        frame.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent me) {
+                for (ParkingBlock block : blocksList) {
+                    if (me.getX() >= block.getX() + 310 && me.getX() <= block.getX() + 330 && me.getY() >= block.getY() + 190 && me.getY() <= block.getY() + 210) {
+                        if (SwingUtilities.isLeftMouseButton(me)) {
+                            block.changeToFreeParking();
+                        } else if (SwingUtilities.isRightMouseButton(me)) {
+                            block.changeToOccupiedParking();
+                        } else {
+                            block.changeToStreet();
+                        }
+                    }
+                }
+                canvas.setBlocksList(blocksList);
+                canvas.updateGraphics();
+            }
+        });
 
         frame.pack();
         frame.setVisible(true);
     }
 
     private static class CanvasPanel extends JPanel {
-        private int rows;
-        private int columns;
+        private List<ParkingBlock> blocksList;
 
-        CanvasPanel(int rows, int columns) {
-            setPreferredSize(new Dimension(1600, 900));
-            this.rows = rows;
-            this.columns = columns;
+        CanvasPanel(List<ParkingBlock> blocksList) {
+            this.blocksList = blocksList;
+        }
+
+        void setBlocksList(List<ParkingBlock> blocksList) {
+            this.blocksList = blocksList;
         }
 
         @Override
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
-            g.setColor(Color.BLACK);
-            int x = 20;
-            int y = 20;
-            for (int i = 0; i < rows; ++i) {
-                x = 20;
-                g.fillRect(x, y, 40, 40);
-                for (int j = 0; j < columns - 1; ++j) {
-                    x += 50;
-                    g.fillRect(x, y, 40, 40);
+            for (ParkingBlock block : blocksList) {
+                if (block.getType() == ParkingBlock.Blocks.STREET) {
+                    g.setColor(Color.BLACK);
+                } else if (block.getType() == ParkingBlock.Blocks.FREEPARKING) {
+                    g.setColor(Color.GREEN);
+                } else {
+                    g.setColor(Color.RED);
                 }
-                y += 50;
+                g.fillRect(block.getX(), block.getY(), 20, 20);
             }
         }
 
